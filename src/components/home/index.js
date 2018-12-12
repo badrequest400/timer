0cbb25c94a488c2817df240438cf7b406f1e3760
Balazs Cseh <balazs.cseh89@gmail.com>
2018-12-12 01:24:04 +0000

    bring in context provider

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Button } from 'react-native';
 import styles from './styles';
+import { Provider } from '../context';
 
 const sevenSeconds = new Date(new Date(null).setSeconds(7))
 
@@ -8,18 +9,20 @@ export default class Home extends Component {
 
   render() {
     const { navigation: { navigate } } = this.props;
-
+    const state = {};
     return (
-      <View style={styles.container}>
-        <Button
-          title="Load"
-          onPress={() => navigate('Timer', { current: sevenSeconds })}
-        />
-        <Button
-          title="New workout"
-          onPress={() => navigate('Designer')}
-        />
-      </View>
+      <Provider value={state}>
+        <View style={styles.container}>
+          <Button
+            title="Load"
+            onPress={() => navigate('Timer', { current: sevenSeconds })}
+          />
+          <Button
+            title="New workout"
+            onPress={() => navigate('Designer')}
+          />
+        </View>
+      </Provider>
     )
   }
 }
